c1354bfa84cc02e3ff6e44537d3334ca90cd9d5a
CodeWithVinutha <vinuthaj27@gmail.com>
2025-07-17 14:22:12 +0530

    program-2

---
+++ screen orientation/app/src/main/java/com/example/screen_orientation/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.screen_orientation;
+import android.os.Bundle;
+import android.content.res.Configuration;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView msg = findViewById(R.id.orientationMessage);
+
+        // Check orientation and set message
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            msg.setText("Landscape Mode");
+        } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            msg.setText("Portrait Mode");
+        } else {
+            msg.setText("Unknown Orientation");
+        }
+    }
+}
